56d04745d9c99866eb0a3f0c4f7eecc7dac5a5e8
Headless Granules <hgranule@at-g4.21-school.ru>
2019-05-27 11:24:04 +0300

    comments added

--- ls_main.c
+++ ls_main.c
@@ -6,7 +6,7 @@
 /*   By: hgranule <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 11:47:43 by hgranule          #+#    #+#             */
-/*   Updated: 2019/05/27 10:58:58 by hgranule         ###   ########.fr       */
+/*   Updated: 2019/05/27 11:23:49 by hgranule         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ t_flags		g_flags;
 
 int		main(int ac, char **av)
 {
-
+	// NOTE parsing input av into flags (g_flags) and arguments.
+	// NOTE think about RECURSIVE architecture
 	return (0);
 }
